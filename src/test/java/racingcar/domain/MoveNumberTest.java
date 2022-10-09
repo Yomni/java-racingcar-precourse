335c424e42bb3a9e9a3ab9919dd3c11fa20fb193
Yomni <yearsheep@daum.net>
2022-10-09 21:06:04 +0900

    test: MoveNumber 테스트 추가

--- src/test/java/racingcar/domain/MoveNumberTest.java
+++ src/test/java/racingcar/domain/MoveNumberTest.java
@@ -7,9 +7,8 @@ import java.time.temporal.ValueRange;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoveNumberTest {
-
     @Test
-    void name() {
+    void 랜덤_MoveNumber_범위테스트() {
         int moveNumberRandomly = new MoveNumber().getMoveNumber();
         ValueRange boundary = ValueRange.of(0, 9);
 
